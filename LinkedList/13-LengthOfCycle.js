04056956bf776ea9854358038e1fddadc1dbad97
palakurthiprashanth <palakurthi.prashanth1@gmail.com>
2021-05-12 15:35:09 +0530

    length of cycle

---
+++ LinkedList/13-LengthOfCycle.js
@@ -0,0 +1,30 @@
+function lengthOfCycle(list) {
+    let currentNode= list.head;
+
+    let slow= currentNode;
+    let fast= currentNode;
+
+    while(slow && fast && fast.nextElement) {
+        slow= slow.nextElement;
+        fast= fast.nextElement.nextElement;
+
+        if (slow === fast) {
+            return calculateCycleLength(slow);
+        }
+    }
+    return "no cycle";
+}
+
+function calculateCycleLength(slow) {
+    let current= slow;
+    let length= 0;
+
+    while(current!==null) {
+        current= current.nextElement;
+        length+=1;
+        if (current === slow) {
+            break;
+        }
+    }
+    return length;
+}
