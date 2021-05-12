e896b822427d3bbdfc93e5d734ce27b59e3b734d
palakurthiprashanth <palakurthi.prashanth1@gmail.com>
2021-05-12 15:36:15 +0530

    2 files modified

--- LinkedList/13-LengthOfCycle.js
+++ LinkedList/13-LengthOfCycle.js
@@ -1,30 +1,30 @@
 function lengthOfCycle(list) {
-    let currentNode= list.head;
+  let currentNode = list.head;
 
-    let slow= currentNode;
-    let fast= currentNode;
+  let slow = currentNode;
+  let fast = currentNode;
 
-    while(slow && fast && fast.nextElement) {
-        slow= slow.nextElement;
-        fast= fast.nextElement.nextElement;
+  while (slow && fast && fast.nextElement) {
+    slow = slow.nextElement;
+    fast = fast.nextElement.nextElement;
 
-        if (slow === fast) {
-            return calculateCycleLength(slow);
-        }
+    if (slow === fast) {
+      return calculateCycleLength(slow);
     }
-    return "no cycle";
+  }
+  return 'no cycle';
 }
 
 function calculateCycleLength(slow) {
-    let current= slow;
-    let length= 0;
+  let current = slow;
+  let length = 0;
 
-    while(current!==null) {
-        current= current.nextElement;
-        length+=1;
-        if (current === slow) {
-            break;
-        }
+  while (current !== null) {
+    current = current.nextElement;
+    length += 1;
+    if (current === slow) {
+      break;
     }
-    return length;
+  }
+  return length;
 }
